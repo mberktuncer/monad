6ee6d2dba689c38b10be7ae7d914d71b6a678cba
Berk <mberktuncer@gmail.com>
2025-02-12 21:10:35 +0300

    visual improvements have been made

--- monad/src/main/java/com/mberktuncer/monad/views/EmployeeView.java
+++ monad/src/main/java/com/mberktuncer/monad/views/EmployeeView.java
@@ -9,6 +9,11 @@ import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 
 import java.util.List;
 
@@ -26,21 +31,32 @@ public class EmployeeView extends VerticalLayout {
         TextField searchField = new TextField();
         searchField.setPlaceholder("İsim ile ara...");
         searchField.setWidth("300px");
+        searchField.setPrefixComponent(new Icon(VaadinIcon.SEARCH));
+        searchField.setClearButtonVisible(true);
         searchField.setValueChangeMode(ValueChangeMode.LAZY);
 
         grid = new Grid<>(Employee.class);
-        grid.setColumns("TC", "ad", "soyad");
+        grid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+        grid.setHeight("400px");
 
         List<Employee> employees = employeeService.findAll();
 
         dataProvider = new ListDataProvider<>(employees);
         grid.setDataProvider(dataProvider);
 
-        grid.getColumnByKey("TC").setHeader("TC No");
-        grid.getColumnByKey("ad").setHeader("Ad");
-        grid.getColumnByKey("soyad").setHeader("Soyad");
+        grid.removeAllColumns();
+        grid.addColumn(Employee::getTC)
+            .setHeader("TC No")
+            .setWidth("150px")
+            .setFlexGrow(0);
         
-        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+        grid.addColumn(Employee::getAd)
+            .setHeader("Ad")
+            .setSortable(true);
+            
+        grid.addColumn(Employee::getSoyad)
+            .setHeader("Soyad")
+            .setSortable(true);
 
         searchField.addValueChangeListener(e -> {
             String filterText = e.getValue().trim();
