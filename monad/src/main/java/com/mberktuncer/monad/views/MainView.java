52e8d645a787165d38c0e768e92e56354e75ed16
Berk <mberktuncer@gmail.com>
2025-02-13 09:58:12 +0300

    modularized MainView class.

--- monad/src/main/java/com/mberktuncer/monad/views/MainView.java
+++ monad/src/main/java/com/mberktuncer/monad/views/MainView.java
@@ -22,79 +22,112 @@ public class MainView extends AppLayout {
     private H1 viewTitle;
 
     public MainView() {
+        this.menu = new Tabs();
+        initializeLayout();
+        initializeMenu();
+    }
+
+    private void initializeLayout() {
         setPrimarySection(Section.DRAWER);
-
         addToNavbar(true, createHeaderContent());
+    }
 
-        menu = createMenu();
+    private void initializeMenu() {
+        configureMenuTabs(menu);
         addToDrawer(createDrawerContent(menu));
-
     }
 
     private Component createHeaderContent() {
-        HorizontalLayout layout = new HorizontalLayout();
-
-        layout.setId(MainViewEnums.HEADER_LAYOUT_ID.getText());
-
-        layout.setWidthFull();
-        layout.setSpacing(false);
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        layout.add(new DrawerToggle());
-
-        viewTitle = new H1();
-        layout.add(viewTitle);
-
+        HorizontalLayout layout = createHeaderLayout();
+        addDrawerToggleToHeader(layout);
+        addViewTitleToHeader(layout);
         return layout;
     }
 
-    private Component createDrawerContent(Tabs menu) {
-        VerticalLayout layout = new VerticalLayout();
+    private HorizontalLayout createHeaderLayout() {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setId(MainViewEnums.HEADER_LAYOUT_ID.getText());
+        layout.setWidthFull();
+        layout.setSpacing(false);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        return layout;
+    }
 
+    private void addDrawerToggleToHeader(HorizontalLayout layout) {
+        layout.add(new DrawerToggle());
+    }
+
+    private void addViewTitleToHeader(HorizontalLayout layout) {
+        viewTitle = new H1();
+        layout.add(viewTitle);
+    }
+
+    private Component createDrawerContent(Tabs menu) {
+        VerticalLayout layout = createDrawerLayout();
+        HorizontalLayout logoLayout = createLogoLayout();
+        layout.add(logoLayout, menu);
+        return layout;
+    }
+
+    private VerticalLayout createDrawerLayout() {
+        VerticalLayout layout = new VerticalLayout();
+        configureDrawerLayout(layout);
+        return layout;
+    }
+
+    private void configureDrawerLayout(VerticalLayout layout) {
         layout.setSizeFull();
         layout.setPadding(false);
         layout.setSpacing(false);
         layout.getThemeList().set(MainViewEnums.DRAWER_THEME.getText(), true);
         layout.setAlignItems(FlexComponent.Alignment.STRETCH);
+    }
 
+    private HorizontalLayout createLogoLayout() {
         HorizontalLayout logoLayout = new HorizontalLayout();
         logoLayout.setId(MainViewEnums.DRAWER_LAYOUT_ID.getText());
         logoLayout.add(new H1(MainViewEnums.DRAWER_LAYOUT_H1.getText()));
-
-        layout.add(logoLayout, menu);
-        return layout;
+        return logoLayout;
     }
 
-    private Tabs createMenu() {
-        final Tabs tabs = new Tabs();
+    private void configureMenuTabs(Tabs tabs) {
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
         tabs.setId(MainViewEnums.MENU_ID.getText());
         tabs.add(createMenuItems());
-        return tabs;
     }
 
-    private Component[] createMenuItems() {
+    private Tab[] createMenuItems() {
         return new Tab[] {
-                createTab(MainViewEnums.TAB_HELLO_WORLD.getText(), HelloWorldView.class),
-                createTab(MainViewEnums.TAB_EMPLOYEES.getText(), EmployeeView.class),
-                createTab(MainViewEnums.TAB_ABOUT.getText(), AboutView.class) };
+            createTab(MainViewEnums.TAB_HELLO_WORLD.getText(), HelloWorldView.class),
+            createTab(MainViewEnums.TAB_EMPLOYEES.getText(), EmployeeView.class),
+            createTab(MainViewEnums.TAB_ABOUT.getText(), AboutView.class)
+        };
     }
 
-    private static Tab createTab(String text,
-                                 Class<? extends Component> navigationTarget) {
+    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
         final Tab tab = new Tab();
+        configureTab(tab, text, navigationTarget);
+        return tab;
+    }
+
+    private static void configureTab(Tab tab, String text, Class<? extends Component> navigationTarget) {
         tab.add(new RouterLink(text, navigationTarget));
         ComponentUtil.setData(tab, Class.class, navigationTarget);
-        return tab;
     }
 
     @Override
     protected void afterNavigation() {
         super.afterNavigation();
+        updateSelectedTab();
+        updateViewTitle();
+    }
 
+    private void updateSelectedTab() {
         getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+    }
 
+    private void updateViewTitle() {
         viewTitle.setText(getCurrentPageTitle());
     }
 
@@ -108,5 +141,4 @@ public class MainView extends AppLayout {
     private String getCurrentPageTitle() {
         return getContent().getClass().getAnnotation(PageTitle.class).value();
     }
-
 }
