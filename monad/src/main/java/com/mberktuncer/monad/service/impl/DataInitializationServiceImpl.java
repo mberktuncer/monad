b2c5810aed8b1d607aad058f70261bf76faaf884
Berk <mberktuncer@gmail.com>
2025-02-13 14:36:37 +0300

    Data initialize method changed. DataInitializationService added.

---
+++ monad/src/main/java/com/mberktuncer/monad/service/impl/DataInitializationServiceImpl.java
@@ -0,0 +1,41 @@
+package com.mberktuncer.monad.service.impl;
+
+import com.mberktuncer.monad.model.Employee;
+import com.mberktuncer.monad.repository.EmployeeRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@Service
+public class DataInitializationServiceImpl {
+
+    private final EmployeeRepository employeeRepository;
+
+    public DataInitializationServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostConstruct
+    public void initializeEmployees() {
+        if (employeeRepository.count() == 0) {
+            try {
+                ClassPathResource resource = new ClassPathResource("employees.txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+                
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split(",");
+                    Employee employee = new Employee(data[0].trim(), data[1].trim(), data[2].trim());
+                    employeeRepository.save(employee);
+                }
+                
+                reader.close();
+            } catch (Exception e) {
+                throw new RuntimeException("Something went wrong when employees importing", e);
+            }
+        }
+    }
+} 
--- monad/src/main/java/com/mberktuncer/monad/service/impl/EmployeeServiceImpl.java
+++ monad/src/main/java/com/mberktuncer/monad/service/impl/EmployeeServiceImpl.java
@@ -3,12 +3,8 @@ package com.mberktuncer.monad.service.impl;
 import com.mberktuncer.monad.model.Employee;
 import com.mberktuncer.monad.repository.EmployeeRepository;
 import com.mberktuncer.monad.service.contract.EmployeeService;
-import jakarta.annotation.PostConstruct;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.List;
 
 @Service
@@ -18,27 +14,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
-    }
-
-    @PostConstruct
-    public void initializeEmployees() {
-        if (employeeRepository.count() == 0) {
-            try {
-                ClassPathResource resource = new ClassPathResource("employees.txt");
-                BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
-                
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] data = line.split(",");
-                    Employee employee = new Employee(data[0].trim(), data[1].trim(), data[2].trim());
-                    employeeRepository.save(employee);
-                }
-                
-                reader.close();
-            } catch (Exception e) {
-                throw new RuntimeException("Something went wrong when employees importing", e);
-            }
-        }
     }
 
     @Override
